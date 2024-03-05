a636899557034cd4686d0ae885b01137746f674c
dongshouyang <dongshouyang@cambricon.com>
2024-03-05 22:40:54 +0800

    add cpp test

--- 8192_8192_transpose.cpp
+++ 8192_8192_transpose.cpp
@@ -1,4 +1,4 @@
-void transpose_kernel(float*  ins, float*  outs) {
+extern "C" void transpose_kernel(float*  ins, float*  outs) {
     for (int i = 0; i < 8192; ++i) {
         for (int j = 0; j < 8192; ++j) {
             outs[j * 8192 + i] = ins[i * 8192 + j];
--- test_transpose_cpp.py
+++ test_transpose_cpp.py
@@ -15,48 +15,19 @@ def run_compilation(so_name, file_name):
             stderr=subprocess.STDOUT,
             encoding="utf-8",
             check=True,
-            text=True,
             timeout=15,
         )
         return True, output
     except subprocess.CalledProcessError as e:
         return False, e.output
 
-
-def softmax(x):
-    # 对最后一个维度进行softmax操作
-    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
-    return e_x / np.sum(e_x, axis=-1, keepdims=True)
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file", help="the source file")
-    parser.add_argument('--conversion', action='store_true', help='A boolean argument')
-    args = parser.parse_args()
-    base_name = os.path.basename(args.file)
-    name = base_name.split("_")[0]
-    shape = base_name.split("_")[1:-1]
-    shape = [int(intg) for intg in shape]
-    so_name = args.file.replace(".cpp", ".so")
-    dtype = base_name.split("_")[-1].replace(".cpp", "")
-    with open(args.file, "r") as f:
-        code = f.read()
-        f.close()
-
-    with open("./macro/cpp_macro.txt", "r") as f:
-        macro = f.read()
-        f.close()
-    code = macro + code
-    code = code.replace("kernel0(", "kernel(")
-    file_name = args.file.replace(base_name.replace(".cpp", ""), base_name + "_bak.cpp")
-    with open(file_name, mode="w") as f:
-        f.write(code)
-        f.close()
-
-
+    name = "transpose"
+    shape = [8192, 8192]
+    file_name = "8192_8192_transpose.cpp"
+    so_name = "8192_8192_transpose.so"
+    
     success, output = run_compilation(so_name, file_name)
-
-    os.remove(file_name)
     lib = CDLL(so_name)
     function = getattr(lib, name + "_kernel")
     # 定义函数参数和返回类型
@@ -66,8 +37,9 @@ if __name__ == "__main__":
     ]
     function.restype = None
     # 创建输入数组
+    dtype = "float32"
     input_array = np.random.uniform(size=shape).astype(dtype)
-    expected_output = softmax(input_array)
+    expected_output = np.transpose(input_array)
 
     # 创建输出数组
     output_array = np.zeros_like(input_array)
@@ -96,7 +68,4 @@ if __name__ == "__main__":
         function(input_ptr, output_ptr)    
     t2 = time.time()
     cost = (t2 - t1) / 20.0 * 1e3
-    with open(args.file.replace(".cpp", ".txt"), mode="w") as f:
-        f.write(str(cost))
-        f.close()
-    result = subprocess.run(["rm", so_name])
+    print("[INFO]*******cost: ", cost)
