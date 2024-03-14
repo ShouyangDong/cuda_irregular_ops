81f2915617e0d807c5abd816bf1acbb337940729
Shouyang Dong <shouyang.dong@gmail.com>
2024-03-14 11:30:54 +0800

    add mlu layer norm kernel

---
+++ test_layer_norm_mlu.py
@@ -0,0 +1,78 @@
+import numpy as np
+from ctypes import CDLL, c_void_p, c_double, c_int
+import subprocess
+import glob
+import os
+import ctypes
+import argparse
+
+def run_compilation(so_name, file_name):
+    try:
+        output = subprocess.run(
+            ["cncc", "-shared", "-Xcompiler", "-fPIC", "-o", so_name, file_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding="utf-8",
+            check=True,
+            text=True,
+            timeout=15,
+        )
+        return True, output
+    except subprocess.CalledProcessError as e:
+        return False, e.output
+
+def ref_program(x, gamma, beta, eps=1e-5):
+    mean = np.mean(x, axis=-1, keepdims=True)
+    std = np.std(x, axis=-1, keepdims=True)
+    x_normalized = (x - mean) / (std + eps)
+    out = gamma * x_normalized + beta
+    return out
+
+
+
+if __name__ == "__main__":
+    name = "layer_norm"
+    shape = [2, 4, 8]
+    file_name = "layer_norm.cu"
+    so_name = "layer_norm_gpu.so"
+    
+    success, output = run_compilation(so_name, file_name)
+    lib = CDLL(os.path.join(os.getcwd(), so_name))
+    function = getattr(lib, name + "_kernel")
+    # 定义函数参数和返回类型
+    function.argtypes = [
+        ctypes.POINTER(ctypes.c_float),
+        ctypes.POINTER(ctypes.c_float),
+        ctypes.POINTER(ctypes.c_float),
+        ctypes.POINTER(ctypes.c_float)
+    ]
+    function.restype = None
+    # 创建输入数组
+    dtype = "float32"
+    input_array = np.random.uniform(size=shape).astype(dtype)
+    gamma_array = np.random.uniform(size=shape[-1:]).astype(dtype)
+    beta_array = np.random.uniform(size=shape[-1:]).astype(dtype)
+    expected_output = ref_program(input_array, gamma_array, beta_array)
+
+    # 创建输出数组
+    output_array = np.zeros_like(input_array)
+
+    # 将输入数组和输出数组转换为C指针类型
+    input_ptr = input_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    gamma_ptr = gamma_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    beta_ptr = beta_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    output_ptr = output_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    # 调用C函数
+    function(input_ptr, gamma_ptr, beta_ptr, output_ptr)
+    # 验证结果
+
+    np.testing.assert_allclose(
+        output_array,
+        expected_output,
+        rtol=1e-03,
+        atol=1e-03,
+        equal_nan=True,
+        err_msg="",
+        verbose=True,
+    )
+    print("验证通过！")
