7ea7b57a2f6f56c3a35d4d0932d2b4e9e325eac4
shouyang <you@example.com>
2024-03-22 10:36:27 +0800

    remove useless cpp file

--- 8192_8192_transpose.cpp
+++
@@ -1,7 +0,0 @@
-extern "C" void transpose_kernel(float*  ins, float*  outs) {
-    for (int i = 0; i < 8192; ++i) {
-        for (int j = 0; j < 8192; ++j) {
-            outs[j * 8192 + i] = ins[i * 8192 + j];
-        }
-    }
-}
