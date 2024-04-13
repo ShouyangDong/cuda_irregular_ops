7753b90fabbebd590da6c16f02aa074f6e2bfba5
dongshouyang <dongshouyang@cambricon.com>
2024-04-13 15:34:40 +0800

    add op gemv

--- test_gemv.py
+++ test_gemv.py
@@ -18,31 +18,30 @@ def run_compilation(so_name, file_name):
     except subprocess.CalledProcessError as e:
         return False, e.output
 
-# Define the batch matrix multiplication function using numpy
-def batch_matmul(A, B):
-    return np.matmul(A, B)
 
 # Generate random matrices for testing
-batch_size = 3
-matrix_dim_i = 4
-matrix_dim_j = 5
-matrix_dim_k = 6
-A = np.random.rand(batch_size, matrix_dim_i, matrix_dim_j).astype("float32")
-B = np.random.rand(batch_size, matrix_dim_j, matrix_dim_k).astype("float32")
+# Define the input matrix A and vector x
+A = np.random.rand(3, 4).astype(np.float32)
+x = np.random.rand(4).astype(np.float32)
 
-# Perform batch matrix multiplication using numpy
-result_np = batch_matmul(A, B)
+# Create an empty vector y
+y_ctypes = np.zeros(3, dtype=np.float32)
 
 # Convert the matrices to contiguous memory for ctypes
 A_ptr = A.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
-B_ptr = B.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
-file_name = "bmm.cpp"
-so_name = "bmm.so"
+x_ptr = x.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+y_ptr = y_ctypes.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+
+# Perform gemv using numpy
+y_np = np.matmul(A, x)
+
+file_name = "gemv.cpp"
+so_name = "gemv.so"
 # Load the shared library with the batch matrix multiplication function
 success, output = run_compilation(so_name, file_name)
 # # os.remove(file_name)
 lib = ctypes.CDLL(os.path.join(os.getcwd(), so_name))
-function = getattr(lib, "bmm_kernel")
+function = getattr(lib, "gemv_kernel")
 # 定义函数参数和返回类型
 function.argtypes = [
     ctypes.POINTER(ctypes.c_float),
@@ -51,13 +50,11 @@ function.argtypes = [
 ]
 function.restype = None
 # Call the function with the matrices and dimensions
-result_ctypes = np.zeros((batch_size, matrix_dim_i, matrix_dim_k), dtype=np.float32)
-output_ptr = result_ctypes.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
-function(output_ptr, A_ptr, B_ptr)
+function(y_ptr, A_ptr, x_ptr)
 # Check if the results match
 np.testing.assert_allclose(
-    result_ctypes,
-    result_np,
+    y_ctypes,
+    y_np,
     rtol=1e-03,
     atol=1e-03,
     equal_nan=True,
