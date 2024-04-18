53e66d97dcf9a59d819fd8833ac834b3889989ba
shouyang <you@example.com>
2024-04-18 11:10:27 +0800

    fix prefix

--- dl_boost_test/bmm_1_32_128_128.cpp
+++ dl_boost_test/bmm_1_32_128_128.cpp
@@ -17,10 +17,10 @@ extern "C" void bmm_kernel(float *result, float *A, float *B) {
                         arr_src[i_src] = (uint32_t)0;
                     }
                 
-                __128i A = _mm_loadu_si128((__128i*)&arr_a);
-                __128i B = _mm_loadu_si128((__128i*)&arr_b);
-                __128i src = _mm_loadu_si128((__128i*)&arr_src);
-                __128i local_result =  _mm_dpbusds_epi32(src, A, B);
+                __m128i A = _mm_loadu_si128((__m128i*)&arr_a);
+                __m128i B = _mm_loadu_si128((__m128i*)&arr_b);
+                __m128i src = _mm_loadu_si128((__m128i*)&arr_src);
+                __m128i local_result =  _mm_dpbusds_epi32(src, A, B);
                 uint32_t *val = (uint32_t*) &local_result;
                 for(int i = 0; i < 4; i++){
                  sum += val[1];
