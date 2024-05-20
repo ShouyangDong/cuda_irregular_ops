b8a426f43f1888cc6116dfd0b653faa814a188d7
ShouyangDong <shouyang.dong@gmail.com>
2024-05-20 16:45:51 +0800

    add support for cuda conv1d

---
+++ cuda_code_test/conv1d_222_224.cu
@@ -0,0 +1,40 @@
+__global__ void conv1d_kernel(float *input, float *kernel, float *output) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int output_idx = idx + 3 / 2;
+
+    if (idx < 224) {
+        float sum = 0.0f;
+        for (int i = 0; i < 3; i++) {
+            int input_idx = idx + i - 3/2;
+            if (input_idx >= 0 && input_idx < 224) {
+                sum += input[input_idx] * kernel[i];
+            }
+        }
+        output[output_idx] = sum;
+    }
+}
+
+
+void conv1d(float *input, float *kernel, float *output) {
+    int input_size = 224;
+    int kernel_size = 3;
+    float *d_input, *d_kernel, *d_output;
+
+    cudaMalloc(&d_input, input_size * sizeof(float));
+    cudaMalloc(&d_kernel, kernel_size * sizeof(float));
+    cudaMalloc(&d_output, input_size * sizeof(float));
+
+    cudaMemcpy(d_input, input, input_size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_kernel, kernel, kernel_size * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 blockSize(224);
+    dim3 numBlocks((input_size + blockSize.x - 1) / blockSize.x);
+
+    conv1d_kernel<<<numBlocks, blockSize>>>(d_input, d_kernel, d_output);
+
+    cudaMemcpy(output, d_output, input_size * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_input);
+    cudaFree(d_kernel);
+    cudaFree(d_output);
+}
