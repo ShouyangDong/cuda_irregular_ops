3635f55d1a14f42ed1966f2e7fd6ad2151ae2989
ShouyangDong <shouyang.dong@gmail.com>
2024-05-21 11:02:40 +0800

    add support for cuda test

---
+++ cuda_test/result_test.py
@@ -0,0 +1,54 @@
+import subprocess
+import glob
+import os
+from tqdm import tqdm
+
+def run_test(file_name, test_file):
+    try:
+        output = subprocess.run(
+            ["python", test_file, "--file", file_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding="utf-8",
+            check=True,
+            text=True,
+            timeout=400,
+        )           
+        return True, output
+    except subprocess.TimeoutExpired:
+        return False, "timeout"
+    except subprocess.CalledProcessError as e:
+        return False, e.output
+
+if __name__ == "__main__":
+    files = glob.glob("./cuda_code_test/*.cu")
+    counter = 0
+
+    for file in tqdm(files):
+        base_name = os.path.basename(file)
+        name = base_name.split("_")[0]
+        if name == "deformable":
+            success, output = run_test(file, "./cuda_test/test_deformable_attention_cuda.py")
+        elif name == "layernorm":
+            success, output = run_test(file, "./cuda_test/test_layer_norm_cuda.py")
+        elif name == "mha":
+            success, output = run_test(file, "./cuda_test/test_mha_cuda.py")
+        elif name == "rmsnorm":
+            success, output = run_test(file, "./cuda_test/test_rms_norm_cuda.py")
+        elif name == "gemm":
+            success, output = run_test(file, "./cuda_test/test_gemm.py")
+        elif name == "gemv":
+            success, output = run_test(file, "./cuda_test/test_gemv.py")
+        elif name == "bmm":
+            success, output = run_test(file, "./cuda_test/test_bmm.py")
+
+        if hasattr(output, "stdout") and "验证通过" in output.stdout:
+            counter += 1
+
+        elif isinstance(output, str):
+            print(base_name)
+            print(output)
+
+    print(counter)
+    print(len(files))
+    print("[INFO]*******************cuda test successfule rate: ",  counter / len(files))
