1a4936fa659a738757962f76ec5f1f92a7bd4b03
ShouyangDong <shouyang.dong@gmail.com>
2024-05-21 11:16:30 +0800

    add support for cuda conv1d test

--- cuda_test/result_test.py
+++ cuda_test/result_test.py
@@ -41,6 +41,8 @@ if __name__ == "__main__":
             success, output = run_test(file, "./cuda_test/test_gemv.py")
         elif name == "bmm":
             success, output = run_test(file, "./cuda_test/test_bmm.py")
+        elif name == "conv1d":
+            success, output = run_test(file, "./cuda_test/test_conv1d.py")
 
         if hasattr(output, "stdout") and "验证通过" in output.stdout:
             counter += 1
