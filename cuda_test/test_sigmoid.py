dd74a052f903683a45975b51ffaea85d1633e0c1
ShouyangDong <shouyang.dong@gmail.com>
2024-05-27 23:01:29 +0800

    add support for cuda test file

---
+++ cuda_test/test_sigmoid.py
@@ -0,0 +1,70 @@
+import numpy as np
+from ctypes import CDLL, c_void_p, c_double, c_int
+import subprocess
+import glob
+import os
+import ctypes
+import argparse
+import torch
+
+
+def run_compilation(so_name, file_name):
+    try:
+        output = subprocess.run(
+            ["nvcc", "-shared", "-Xcompiler", "-fPIC", "-o", so_name, file_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding="utf-8",
+            check=True,
+            text=True,
+            timeout=15,
+        )
+        return True, output
+    except subprocess.CalledProcessError as e:
+        return False, e.output
+
+
+def ref_program(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    name = "rms_norm"
+    shape = [8192, 8192]
+    file_name = "rms_norm.cu"
+    so_name = "rms_norm_cuda.so"
+
+    success, output = run_compilation(so_name, file_name)
+    lib = CDLL(os.path.join(os.getcwd(), so_name))
+    function = getattr(lib, name + "_kernel")
+    # 定义函数参数和返回类型
+    function.argtypes = [
+        ctypes.POINTER(ctypes.c_float),
+        ctypes.POINTER(ctypes.c_float),
+    ]
+    function.restype = None
+    # 创建输入数组
+    dtype = "float32"
+    input_array = np.random.uniform(size=shape).astype(dtype)
+    expected_output = ref_program(input_array)
+
+    # 创建输出数组
+    output_array = np.zeros_like(input_array)
+
+    # 将输入数组和输出数组转换为C指针类型
+    input_ptr = input_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    output_ptr = output_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    # 调用C函数
+    function(input_ptr, output_ptr)
+    # 验证结果
+
+    np.testing.assert_allclose(
+        output_array,
+        expected_output,
+        rtol=1e-03,
+        atol=1e-03,
+        equal_nan=True,
+        err_msg="",
+        verbose=True,
+    )
+
+    print("验证通过！")
---
+++ cuda_test/test_softmax.py
@@ -0,0 +1,71 @@
+import numpy as np
+from ctypes import CDLL, c_void_p, c_double, c_int
+import subprocess
+import glob
+import os
+import ctypes
+import argparse
+import torch
+
+
+def run_compilation(so_name, file_name):
+    try:
+        output = subprocess.run(
+            ["nvcc", "-shared", "-Xcompiler", "-fPIC", "-o", so_name, file_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding="utf-8",
+            check=True,
+            text=True,
+            timeout=15,
+        )
+        return True, output
+    except subprocess.CalledProcessError as e:
+        return False, e.output
+
+
+def ref_program(x):
+    # 对最后一个维度进行softmax操作
+    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+if __name__ == "__main__":
+    name = "rms_norm"
+    shape = [8192, 8192]
+    file_name = "rms_norm.cu"
+    so_name = "rms_norm_cuda.so"
+
+    success, output = run_compilation(so_name, file_name)
+    lib = CDLL(os.path.join(os.getcwd(), so_name))
+    function = getattr(lib, name + "_kernel")
+    # 定义函数参数和返回类型
+    function.argtypes = [
+        ctypes.POINTER(ctypes.c_float),
+        ctypes.POINTER(ctypes.c_float),
+    ]
+    function.restype = None
+    # 创建输入数组
+    dtype = "float32"
+    input_array = np.random.uniform(size=shape).astype(dtype)
+    expected_output = ref_program(input_array)
+    # 创建输出数组
+    output_array = np.zeros_like(input_array)
+
+    # 将输入数组和输出数组转换为C指针类型
+    input_ptr = input_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    output_ptr = output_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    # 调用C函数
+    function(input_ptr, output_ptr)
+    # 验证结果
+
+    np.testing.assert_allclose(
+        output_array,
+        expected_output,
+        rtol=1e-03,
+        atol=1e-03,
+        equal_nan=True,
+        err_msg="",
+        verbose=True,
+    )
+
+    print("验证通过！")
