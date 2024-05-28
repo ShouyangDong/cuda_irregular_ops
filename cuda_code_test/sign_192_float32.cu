8d7ea7fc7d66e433a9959cd45383f4809c5d6029
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 14:58:42 +0800

    remove useless cuda kernel

--- cuda_code_test/sign_192_float32.cu
+++
@@ -1,3 +0,0 @@
-extern "C" __global__ void __launch_bounds__(192) sign_kernel(float* __restrict__ A, float* __restrict__ T_sign) {
-  T_sign[((int)threadIdx.x)] = ((0.000000e+00f < A[((int)threadIdx.x)]) ? 1.000000e+00f : ((A[((int)threadIdx.x)] < 0.000000e+00f) ? -1.000000e+00f : 0.000000e+00f));
-}
