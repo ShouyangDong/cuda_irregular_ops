208b745da5d0228d0d0a79c55b7116e3d43ee1cd
shouyang <you@example.com>
2024-05-28 15:28:58 +0800

    fix cpp compilation test file

--- cpp_test/compilation.py
+++ cpp_test/compilation.py
@@ -6,12 +6,12 @@ import glob
 def run_compilation(so_name, file_name):
     try:
         output = subprocess.run(
-            ["g++", "-shared", "-fPIC", "-march=icelake-server", "-O3", file_name, "-o", so_name],
+            ["g++", "-shared", "-fPIC", "-O3", file_name, "-o", so_name],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             encoding="utf-8",
             check=True,
-            # text=True,
+            text=True,
             timeout=15,
         )
         return True, output
@@ -19,7 +19,7 @@ def run_compilation(so_name, file_name):
         return False, e.output
 
 
-files = glob.glob("./dl_boost_test/*.cpp")
+files = glob.glob("./cpp_code_test/*.cpp")
 counter = 0
 for file_name in files:
     base_name = os.path.basename(file_name)
@@ -28,20 +28,22 @@ for file_name in files:
         code = f.read()
         f.close()
 
-    with open("./macro/dl_boost_macro.txt", "r") as f:
+    with open("./macro/cpp_macro.txt", "r") as f:
         macro = f.read()
 
     code = macro + code
-    file_name = file_name.replace(base_name.replace(".cpp", ""), base_name + "_bak.cpp")
+    file_name = file_name.replace(".cpp","_bak.cpp")
+
     with open(file_name, mode="w") as f:
         f.write(code)
         f.close()
 
     so_name = base_name.replace("cpp", "so")
-    so_name = os.path.join("./dl_boost_test/", so_name)
+    so_name = os.path.join("./cpp_code_test/", so_name)
 
     success, output = run_compilation(so_name, file_name)
     os.remove(file_name)
+
     if success:
         counter += 1
         result = subprocess.run(["rm", so_name])
