eb1be5a653004b5ed3f0cf92a51275af015d86d6
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 14:37:48 +0800

    modify unit test for cpp kernel

--- cpp_code_test/relu_32_float32.cpp
+++
@@ -1,9 +0,0 @@
-
-extern "C" float reluf(float input) {
-    return input > 0 ? input : 0;
-}
-extern "C" void relu_kernel(float* input, float* output) {
-    for (size_t i = 0; i < 32; i++) {
-        output[i] = reluf(input[i]);
-    }
-}
--- cpp_code_test/sigmoid_7_float32.cpp
+++
@@ -1,9 +0,0 @@
-
-extern "C" float sigmoidf(float input) {
-    return 1 / (1 + exp(-1 * input));
-}
-extern "C" void sigmoid_kernel(float* input, float* output) {
-    for (size_t i = 0; i < 7; i++) {
-        output[i] = sigmoidf(input[i]);
-    }
-}
