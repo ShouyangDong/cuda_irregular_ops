c84b0daeab3f6ac4553ae26ba09fef4b7a05b985
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 14:07:19 +0800

    add support for cpp test

--- cpp_test/result_test.py
+++ cpp_test/result_test.py
@@ -21,7 +21,7 @@ def run_test(file_name, test_file):
         return False, e.output
 
 if __name__ == "__main__":
-    files = glob.glob("./cpp_code_test/gemv*.cpp")
+    files = glob.glob("./cpp_code_test/*.cpp")
     counter = 0
 
     for file in tqdm(files):
@@ -43,6 +43,32 @@ if __name__ == "__main__":
             success, output = run_test(file, "./cpp_test/test_gemv.py")
         elif name == "depthwiseconv":
             success, output = run_test(file, "./cpp_test/test_depthwise_conv.py")
+        elif name == "gemm":
+            success, output = run_test(file, "./cpp_test/test_gemm.py")
+        elif name == "conv2d":
+            success, output = run_test(file, "./cpp_test/test_conv2d.py")
+        elif name == "conv2dNCHW":
+            success, output = run_test(file, "./cpp_test/test_conv2dNCHW.py")
+        elif name == "add":
+            success, output = run_test(file, "./cpp_test/test_add.py")
+        elif name == "sign":
+            success, output = run_test(file, "./cpp_test/test_sign.py")
+        elif name == "avgpool":
+            success, output = run_test(file, "./cpp_test/test_avgpool.py")
+        elif name == "maxpool":
+            success, output = run_test(file, "./cpp_test/test_maxpool.py")
+        elif name == "minpool":
+            success, output = run_test(file, "./cpp_test/test_minpool.py")
+        elif name == "sumpool":
+            success, output = run_test(file, "./cpp_test/test_sumpool.py")
+        elif name == "relu":
+            success, output = run_test(file, "./cpp_test/test_relu.py")
+        elif name == "sigmoid":
+            success, output = run_test(file, "./cpp_test/test_sigmoid.py")
+        elif name == "gelu":
+            success, output = run_test(file, "./cpp_test/test_gelu.py")
+        elif name == "softmax":
+            success, output = run_test(file, "./cpp_test/test_softmax.py")
 
         if hasattr(output, "stdout") and "验证通过" in output.stdout:
             counter += 1
