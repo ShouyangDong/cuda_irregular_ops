462972f90d25e76c13a74d016f2362550c088fa6
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 17:49:43 +0800

    modify the kernel name of conv1d

--- cpp_code_test/conv1d_5_7.cpp
+++ cpp_code_test/conv1d_5_7.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-extern "C" void conv_1d_kernel(float *output, float *input, float *kernel) {
+extern "C" void conv1d_kernel(float *output, float *input, float *kernel) {
     for (int i = 0; i < 5; i++) {
         output[i] = 0;
         for (int j = 0; j < 3; j++) {
