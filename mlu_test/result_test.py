f9dcbc6d7465897125348b868c197bbf301f809a
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 10:59:21 +0800

    add support for mlu test

--- mlu_test/result_test.py
+++ mlu_test/result_test.py
@@ -41,7 +41,34 @@ if __name__ == "__main__":
             success, output = run_test(file, "./mlu_test/test_gemv.py")
         elif name == "bmm":
             success, output = run_test(file, "./mlu_test/test_bmm.py")
-
+        elif name == "conv1d":
+            success, output = run_test(file, "./cuda_test/test_conv1d.py")
+        elif name == "conv2d":
+            success, output = run_test(file, "./cuda_test/test_conv2d.py")
+        elif name = "conv2dNCHW":
+            success, output = run_test(file, "./cuda_test/test_conv2d.py")
+        elif name == "depthwiseconv":
+            success, output = run_test(file, "./cuda_test/test_depthwiseconv.py")
+        elif name == "add":
+            success, output = run_test(file, "./cuda_test/test_add.py")
+        elif name == "sign":
+            success, output = run_test(file, "./cuda_test/test_sign.py")
+        elif name == "avgpool":
+            success, output = run_test(file, "./cuda_test/test_avgpool.py")
+        elif name == "maxpool":
+            success, output = run_test(file, "./cuda_test/test_maxpool.py")
+        elif name == "minpool":
+            success, output = run_test(file, "./cuda_test/test_minpool.py")
+        elif name == "sumpool":
+            success, output = run_test(file, "./cuda_test/test_sumpool.py")
+        elif name == "relu":
+            success, output = run_test(file, "./cuda_test/test_relu.py")
+        elif name == "sigmoid":
+            success, output = run_test(file, "./cuda_test/test_sigmoid.py")
+        elif name == "gelu":
+            success, output = run_test(file, "./cuda_test/test_gelu.py")
+        elif name == "softmax":
+            success, output = run_test(file, "./cuda_test/test_softmax.py")
         if hasattr(output, "stdout") and "验证通过" in output.stdout:
             counter += 1
 
