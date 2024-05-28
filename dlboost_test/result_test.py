af810179955b5841ad99d9c5c8418eaa63068e2a
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 10:34:11 +0800

    add test for dlboost op

--- dlboost_test/result_test.py
+++ dlboost_test/result_test.py
@@ -21,28 +21,54 @@ def run_test(file_name, test_file):
         return False, e.output
 
 if __name__ == "__main__":
-    files = glob.glob("./cpp_code_test/gemv*.cpp")
+    files = glob.glob("./dlboost_code_test/*.cpp")
     counter = 0
 
     for file in tqdm(files):
         base_name = os.path.basename(file)
         name = base_name.split("_")[0]
         if name == "deformable":
-            success, output = run_test(file, "./cpp_test/test_deformable_attention_cpp.py")
+            success, output = run_test(file, "./dlboost_test/test_deformable_attention_cpp.py")
         elif name == "layernorm":
-            success, output = run_test(file, "./cpp_test/test_layer_norm_cpp.py")
+            success, output = run_test(file, "./dlboost_test/test_layer_norm_cpp.py")
         elif name == "mha":
-            success, output = run_test(file, "./cpp_test/test_mha.py")
+            success, output = run_test(file, "./dlboost_test/test_mha.py")
         elif name == "rmsnorm":
-            success, output = run_test(file, "./cpp_test/test_rms_norm_cpp.py")
-        elif name == "bmm":
-            success, output = run_test(file, "./cpp_test/test_bmm.py")
-        elif name == "conv1d":
-            success, output = run_test(file, "./cpp_test/test_conv1d.py")
+            success, output = run_test(file, "./dlboost_test/test_rms_norm_cpp.py")
+        elif name == "gemm":
+            success, output = run_test(file, "./dlboost_test/test_gemm.py")
         elif name == "gemv":
-            success, output = run_test(file, "./cpp_test/test_gemv.py")
+            success, output = run_test(file, "./dlboost_test/test_gemv.py")
+        elif name == "bmm":
+            success, output = run_test(file, "./dlboost_test/test_bmm.py")
+        elif name == "conv1d":
+            success, output = run_test(file, "./dlboost_test/test_conv1d.py")
+        elif name == "conv2d":
+            success, output = run_test(file, "./dlboost_test/test_conv2d.py")
+        elif name == "conv2dNCHW":
+            success, output = run_test(file, "./dlboost_test/test_conv2dNCHW.py")
         elif name == "depthwiseconv":
-            success, output = run_test(file, "./cpp_test/test_depthwise_conv.py")
+            success, output = run_test(file, "./dlboost_test/test_depthwise_conv.py")
+        elif name == "add":
+            success, output = run_test(file, "./dlboost_test/test_add.py")
+        elif name == "sign":
+            success, output = run_test(file, "./dlboost_test/test_sign.py")
+        elif name == "avgpool":
+            success, output = run_test(file, "./dlboost_test/test_avgpool.py")
+        elif name == "maxpool":
+            success, output = run_test(file, "./dlboost_test/test_maxpool.py")
+        elif name == "minpool":
+            success, output = run_test(file, "./dlboost_test/test_minpool.py")
+        elif name == "sumpool":
+            success, output = run_test(file, "./dlboost_test/test_sumpool.py")
+        elif name == "relu":
+            success, output = run_test(file, "./dlboost_test/test_relu.py")
+        elif name == "sigmoid":
+            success, output = run_test(file, "./dlboost_test/test_sigmoid.py")
+        elif name == "gelu":
+            success, output = run_test(file, "./dlboost_test/test_gelu.py")
+        elif name == "softmax":
+            success, output = run_test(file, "./dlboost_test/test_softmax.py")
 
         if hasattr(output, "stdout") and "验证通过" in output.stdout:
             counter += 1
