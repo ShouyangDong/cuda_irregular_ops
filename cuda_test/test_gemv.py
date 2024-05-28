114a0891b97f12a8fb3ebdf188e72939bbf13d75
ShouyangDong <shouyang.dong@gmail.com>
2024-05-28 11:34:25 +0800

    fix kernel name

--- cuda_test/test_gemv.py
+++ cuda_test/test_gemv.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     os.remove(file_name)
     lib = ctypes.CDLL(os.path.join(os.getcwd(), so_name))
-    function = getattr(lib, "gemv_kernel")
+    unction = getattr(lib, name + "_kernel")
     # 定义函数参数和返回类型
     function.argtypes = [
         ctypes.POINTER(ctypes.c_float),
