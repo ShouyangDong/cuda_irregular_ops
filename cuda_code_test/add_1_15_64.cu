ee51f04ed79f7e27cd3430a456a5de84360eee4f
ShouyangDong <shouyang.dong@gmail.com>
2024-05-30 16:14:48 +0800

    add host kernel of add op

--- cuda_code_test/add_1_15_64.cu
+++ cuda_code_test/add_1_15_64.cu
@@ -1,3 +1,26 @@
-extern "C" __global__ void __launch_bounds__(960) add_kernel(float* __restrict__ A, float* __restrict__ B, float* __restrict__ T_add) {
+__global__ void __launch_bounds__(960) add(float* __restrict__ A, float* __restrict__ B, float* __restrict__ T_add) {
   T_add[((int)threadIdx.x)] = (A[((int)threadIdx.x)] + B[((int)threadIdx.x)]);
+}
+
+
+extern "C" void add_kernel(float *C, float *A, float *B, int size) {
+  float *d_A, *d_B, *d_C;
+
+  cudaMalloc(&d_A, size * sizeof(float));
+  cudaMalloc(&d_B, size * sizeof(float));
+  cudaMalloc(&d_C, size * sizeof(float));
+
+  cudaMemcpy(d_A, A, size * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, B, size * sizeof(float), cudaMemcpyHostToDevice);
+
+  dim3 blockSize(128, 128);
+  dim3 numBlocks((m + blockSize.x - 1) / blockSize.x, (n + blockSize.y - 1) / blockSize.y);
+
+  add<<<numBlocks, blockSize>>>(d_A, d_B, d_C);
+
+  cudaMemcpy(C, d_C, m * n * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
 }
