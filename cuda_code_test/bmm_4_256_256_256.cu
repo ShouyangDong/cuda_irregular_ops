742c8f36295fa52ced054e3b546557316f3634c6
ShouyangDong <shouyang.dong@gmail.com>
2024-06-07 17:34:15 +0800

    fix the batch size error of bmm

--- cuda_code_test/bmm_4_256_256_256.cu
+++ cuda_code_test/bmm_4_256_256_256.cu
@@ -15,19 +15,19 @@ __global__ void bmm(float *A, float *B, float *C) {
 extern "C" void bmm_kernel(float *C, float *A, float *B, int b, int m, int k, int n) {
     float *d_A, *d_B, *d_C;
 
-    cudaMalloc(&d_A, m * k * sizeof(float));
-    cudaMalloc(&d_B, k * n * sizeof(float));
-    cudaMalloc(&d_C, m * n * sizeof(float));
+    cudaMalloc(&d_A, b * m * k * sizeof(float));
+    cudaMalloc(&d_B, b * k * n * sizeof(float));
+    cudaMalloc(&d_C, b * m * n * sizeof(float));
 
-    cudaMemcpy(d_A, A, m * k * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, k * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, b * m * k * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, b * k * n * sizeof(float), cudaMemcpyHostToDevice);
 
     dim3 blockSize(32, 32);
     dim3 numBlocks((m + blockSize.x - 1) / blockSize.x, (n + blockSize.y - 1) / blockSize.y);
 
     bmm<<<numBlocks, blockSize>>>(d_A, d_B, d_C);
 
-    cudaMemcpy(C, d_C, m * n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(C, d_C, b * m * n * sizeof(float), cudaMemcpyDeviceToHost);
 
     cudaFree(d_A);
     cudaFree(d_B);
