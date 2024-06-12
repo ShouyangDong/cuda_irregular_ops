e420899e5d0c1585072f0c32d02929e30bb93164
ShouyangDong <shouyang.dong@gmail.com>
2024-06-13 00:17:25 +0800

    fix the split for test

--- test_split_for.py
+++ test_split_for.py
@@ -1,9 +1,9 @@
-from pycparser import c_parser, c_ast
+from pycparser import c_parser, c_ast, c_generator
 
 # C code with a function containing a for loop
 c_code = """
 int factorial(int result) {
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 0; i <= 10; i++) {
         result *= i;
     }
     return result;
@@ -12,45 +12,31 @@ int factorial(int result) {
 
 # Custom visitor class to split 'for' loops with loop count 10 into 2 loops with counts 2 and 5
 class SplitForLoopVisitor(c_ast.NodeVisitor):
+    def __init__(self, axis_name):
+        self.axis_name = axis_name
+
     def visit_For(self, node):
-        if isinstance(node.cond, c_ast.BinaryOp) and \
-            isinstance(node.cond.left, c_ast.ID) and \
-            isinstance(node.cond.op, '<=') and \
-            isinstance(node.cond.right, c_ast.Constant) and \
-            node.cond.right.value == '10':
-            
-            # Modify the existing 'for' loop to have loop count 5
+        # get the loop index:
+        # print(node.init.decls[0].name)
+        if node.init.decls[0].name == self.axis_name:
             node.cond.right.value = '5'
-            node.cond.op = '<'
-            
-            # Create a new 'for' loop with loop count 2
-            new_for_loop = c_ast.For(init=node.init,
-                                     cond=c_ast.BinaryOp('<', c_ast.ID('i'), c_ast.Constant('int', '3')),
-                                     next=c_ast.UnaryOp('++', c_ast.ID('i')),
-                                     stmt=node.stmt)
+            self.visit(node.stmt)
 
-            # Replace the existing 'for' loop with the new one in the parent node
-            parent_node = self.get_parent(node)
-            stmt_index = parent_node.stmt.index(node)
-            parent_node.stmt[stmt_index] = new_for_loop
-
-    def get_parent(self, node):
-        for parent, child in node.parent.items():
-            if child == node:
-                return parent
-        return None
+    def visit_ID(self, node):
+        if node.name == self.axis_name:
+            node.name = self.axis_name + "_out"
 
 # Parse the C code
 parser = c_parser.CParser()
 ast = parser.parse(c_code)
-
+generator = c_generator.CGenerator()
+print(generator.visit(ast))
 # Custom visitor instance
-visitor = SplitForLoopVisitor()
+visitor = SplitForLoopVisitor("i")
 
 # Visit the AST to split 'for' loops with loop count 10 into 2 loops with counts 2 and 5
 visitor.visit(ast)
 
-# Generate the modified C code
-modified_code = ast.show()
-
-print(modified_code)
+# # Generate the modified C code
+# ast.show()
+print(generator.visit(ast))
