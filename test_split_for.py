ac7e8f8c3f7b4ba0e8a184024b01439a2d220d32
ShouyangDong <shouyang.dong@gmail.com>
2024-06-13 09:54:28 +0800

    modify loop split

--- test_split_for.py
+++ test_split_for.py
@@ -12,27 +12,40 @@ int factorial(int result) {
 
 # Custom visitor class to split 'for' loops with loop count 10 into 2 loops with counts 2 and 5
 class SplitForLoopVisitor(c_ast.NodeVisitor):
-    def __init__(self, axis_name):
+    def __init__(self, axis_name, factor):
         self.axis_name = axis_name
+        self.factor = factor
 
     def visit_For(self, node):
         # get the loop index:
-        # print(node.init.decls[0].name)
         if node.init.decls[0].name == self.axis_name:
-            node.cond.right.value = '5'
+            # print(node)
+            node.cond.right.value = int(int(node.cond.right.value) / self.factor)
+            # replace the loop index with post-fix  _out
+            self.visit(node.init)
+            self.visit(node.cond)
+            self.visit(node.next)
             self.visit(node.stmt)
 
     def visit_ID(self, node):
+        # modify the aixs name
         if node.name == self.axis_name:
             node.name = self.axis_name + "_out"
 
+    def visit_Decl(self, node):
+        # modify the init name 
+        if node.name == self.axis_name:
+            print(node)
+            node.name = self.axis_name + "_out"
+            node.type.declname = self.axis_name + "_out"
 # Parse the C code
 parser = c_parser.CParser()
+
 ast = parser.parse(c_code)
 generator = c_generator.CGenerator()
 print(generator.visit(ast))
 # Custom visitor instance
-visitor = SplitForLoopVisitor("i")
+visitor = SplitForLoopVisitor("i", factor=2)
 
 # Visit the AST to split 'for' loops with loop count 10 into 2 loops with counts 2 and 5
 visitor.visit(ast)
