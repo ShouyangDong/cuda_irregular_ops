430023db536159b716031c7d3be0c337605bc4a4
ShouyangDong <shouyang.dong@gmail.com>
2024-06-14 10:18:27 +0800

    modify the gui

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-def transform(source_code, transformation):
+def loop_transform(source_code, transformation):
     if transformation == "Loop Split":
         return "Welcome to Gradio, start to split for loop!"
     elif transformation == "loop fusion":
@@ -9,21 +9,49 @@ def transform(source_code, transformation):
     else:
         return "Welcome to Gradio, start to Recovery for loop!"
 
+def memory_conversion(source_code, transformation):
+    if transformation=="Cache Read":
+        return "Welcome to Gradio, start to Cache Read!"
+    elif transformation=="Cache Write":
+        return "Welcome to Gradio, start to Cache Write!"
+    elif transformation=="Tensor Contraction":
+        return "Welcome to Gradio, start to Tensor Contraction!"
+
+def intrinsic_conversion(source_code, transformation):
+    if transformation == "Tensorization":
+        return "Welcome to Gradio, start to Tensorization!"
+    if transformation == "Detensorization":
+        return "Welcome to Gradio, start to Detensorization!"
 
 title = """<h1 align="center">🔥Falcon: Transcompile Your Code Automatically🚀</h1>"""
 
 with gr.Blocks() as demo:
-    gr.HTML('<center> <img src="/file=falcon.png" style="width: 300px; height: 300px;">')
+    gr.HTML('<center> <img src="/file=falcon.png" style="width: 224px; height: 224px;">')
     gr.HTML(title)
 
-
     inp = gr.Textbox(placeholder="Enter the source code", label="Source code")
+    gr.HTML("""<h1> Sequentialization/Parallelization </h1>""")
     with gr.Row():
         dropdown = gr.Dropdown(choices=["Loop Reorder", "Loop Split", "Loop Fusion", "Loop Recovery"], label="Select a loop transformation option")
+        btn = gr.Button("Run")
         out = gr.Textbox(label="Target code")
 
-    btn = gr.Button("Run")
-    btn.click(fn=transform, inputs=[inp, dropdown], outputs=out)
+    btn.click(fn=loop_transform, inputs=[inp, dropdown], outputs=out)
 
+    gr.HTML("""<h1> Memory Conversion </h1>""")
+    with gr.Row():
+        dropdown = gr.Dropdown(choices=["Cache Read", "Cache Write", "Tensor Contraction"], label="Select a memory conversion option")
+        mem_btn = gr.Button("Run")
+        mem_out = gr.Textbox(label="Target code")
+    
+    mem_btn.click(fn=memory_conversion, inputs=[out, dropdown], outputs=mem_out)
+
+    gr.HTML("""<h1> (De)tensorization </h1>""")
+    with gr.Row():
+        dropdown = gr.Dropdown(choices=["Tensorization", "Detensorization"], label="Select a tensorization option")
+        intrin_btn = gr.Button("Run")
+        intrin_out = gr.Textbox(label="Target code")
+    
+    intrin_btn.click(fn=intrinsic_conversion, inputs=[mem_out, dropdown], outputs=intrin_out)
 
 demo.launch(allowed_paths=["./"])
