bcf57ff0d799e23139b4836f1627e2329ebed858
ShouyangDong <shouyang.dong@gmail.com>
2024-06-18 17:19:32 +0800

    fix the detensorization of smt

--- tensorization/detensorization.py
+++ tensorization/detensorization.py
@@ -1,16 +1,75 @@
 from pycparser import c_parser, c_ast, c_generator
+import json
 
-class FuncCallVisitor(c_ast.NodeVisitor):
-    def __init__(self, funcname):
-        self.funcname = funcname
+class NodeTransformer(c_ast.NodeVisitor):
+    def generic_visit(self, node):
+        for field, old_value in iter_fields(node):
+            if isinstance(old_value, list):
+                new_values = []
+                for value in old_value:
+                    if isinstance(value, c_ast.Node):
+                        value = self.visit(value)
+                        if value is None:
+                            continue
+                        elif not isinstance(value, c_ast.Node):
+                            new_values.extend(value)
+                            continue
+                    new_values.append(value)
+                old_value[:] = new_values
+            elif isinstance(old_value, c_ast.Node):
+                new_node = self.visit(old_value)
+                setattr(node, field, new_node)
+        return node
+
+
+def iter_fields(node):
+    # this doesn't look pretty because `pycparser` decided to have structure
+    # for AST node classes different from stdlib ones
+    index = 0
+    children = node.children()
+    while index < len(children):
+        name, child = children[index]
+        try:
+            bracket_index = name.index("[")
+        except ValueError:
+            yield name, child
+            index += 1
+        else:
+            name = name[:bracket_index]
+            child = getattr(node, name)
+            index += len(child)
+            yield name, child
+
+class Detensorizer(NodeTransformer):
+    def __init__(self, func_defs):
+        self.func_defs = func_defs
+        self.parser = c_parser.CParser()
+        self.parameter_mappings = {}
 
     def visit_FuncCall(self, node):
-        if node.name.name == self.funcname:
-            print("%s called at %s" % (self.funcname, node.name.coord))
+        if node.name.name in self.func_defs:
+            func_def = self.func_defs[node.name.name]
+            seq_def = self.parser.parse(func_def)
+            if not isinstance(seq_def, c_ast.FileAST):
+                raise ValueError("Sequential code must be a function")
 
+            # Construct a map between the function call's  arguments and callee's arguments
+            seq_def_args = seq_def.ext[0].decl.type.args.params
+            seq_def_name = [arg_id.name for arg_id in seq_def_args]
+            self.parameter_mappings = {
+                arg: param for arg, param in zip(seq_def_name, node.args.exprs)
+            }
+            body = seq_def.ext[0].body
+            return self.visit(body)
+        else:
+            return node
 
+    def visit_ID(self, node):
+        if node.name in self.parameter_mappings:
+            return self.parameter_mappings[node.name]
+        return node
 
-def smt_transform(code, loop_index, factor):
+def smt_transform(code, file_name):
     """
     Transform C code using an SMT solver to optimize loop constructs.
 
@@ -21,14 +80,10 @@ def smt_transform(code, loop_index, factor):
 
     Parameters:
     - code (str): A string containing the C code to be transformed.
-    - loop_index (str): The index of the loop to be split in the AST.
-    - factor (int): The factor by which the loop is to be split.
+    - file_name (str): The definition of intrinsics.
 
     Returns:
     - str: The transformed C code as a string.
-
-    Raises:
-    - ValueError: If the loop index is out of bounds or the factor is not valid.
 
     Todo:
     - Implement additional error checking for the input parameters.
@@ -39,10 +94,12 @@ def smt_transform(code, loop_index, factor):
     parser = c_parser.CParser()
     ast = parser.parse(code)
     
+    with open(file_name) as json_file:
+        func_defs = json.load(json_file)
+
     # Create an instance of a visitor that will perform the loop split
     # Assuming SplitForLoopVisitor is a class that knows how to split loops
-    visitor = FuncCallVisitor(loop_index, factor=factor)
-    
+    visitor = Detensorizer(func_defs)
     # Visit the AST with the visitor to apply the transformation
     visitor.visit(ast)
     
@@ -89,3 +146,29 @@ def transform_block(code, user_mannual):
     return code
 
 if __name__ == "__main__":
+    code = """
+    void add_kernel0(float* lhs, float* rhs, float* add_1515) {
+        float lhs_local_nram[128];
+        if (((((int)clusterId) * 4) + ((int)coreId)) < 15) {
+            __memcpy(((float *)lhs_local_nram + (0)), ((float *)lhs + (((((int)clusterId) * 256) + (((int)coreId) * 64)))), 256, GDRAM2NRAM);
+        }
+        if (((((int)clusterId) * 4) + ((int)coreId)) < 15) {
+            __memcpy(((float *)lhs_local_nram + (64)), ((float *)rhs + (((((int)clusterId) * 256) + (((int)coreId) * 64)))), 256, GDRAM2NRAM);
+        }
+        if (((((int)clusterId) * 4) + ((int)coreId)) < 15) {
+            __bang_add(((float *)lhs_local_nram + (0)), ((float *)lhs_local_nram + (0)), ((float *)lhs_local_nram + (64)), 64);
+        }
+        if (((((int)clusterId) * 4) + ((int)coreId)) < 15) {
+            __memcpy(((float *)add_1515 + (((((int)clusterId) * 256) + (((int)coreId) * 64)))), ((float *)lhs_local_nram + (0)), 256, NRAM2GDRAM);
+        }
+    }
+    """
+
+    parser = c_parser.CParser()
+    ast = parser.parse(code)
+    v = FuncCallsRemover(
+        file_name="/Users/dongshouyang/Downloads/micro/cuda_irregular_ops/function_definition.json"
+    )
+    v.visit(ast)
+    generator = c_generator.CGenerator()
+    print(generator.visit(ast))
