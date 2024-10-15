5cc647426d219484d48487fbec0e7f866f58e76a
ShouyangDong <shouyang.dong@gmail.com>
2024-10-15 18:08:05 +0800

    fix preprocessing

--- src/pre_processing/preprocessing.py
+++ src/pre_processing/preprocessing.py
@@ -1,119 +1,38 @@
-import re
-import time
+# autoflake: skip_file
 import openai
 
-from prompt.prompt import (
-    SYSTEM_PROMPT,
-    TASK_PIPELINE_FOR_SINGLE_FUNCTION_PROMPT,
-    TASK_PIPELINE_PROMPT,
-    TASK_PIPELINE_STRATEGY_PROMPT_4,
-)
+from src.pre_processing.preprocessing_prompt import *
 
 
 OPT_LIST = ["LOOP_RECOVERY", "DETENSORIZATION"]
-root_path = "./"
-benchmark_dir = "./benchmark"
-pipeline_log_path = "./log/pipeline"
-
 openai.api_key = """ OPENAI API KEY """
 
 
-# split the whole code
-def gen_task_pipeline_prompt(func_description):
-    _SYSTEM_PROMPT = SYSTEM_PROMPT.replace("{ALGO_NAME}", algo_name)
-    _SYSTEM_PROMPT = _SYSTEM_PROMPT.replace("{FUNCTION_DESCRIPTION}", func_description)
-
-    code_path = f"{benchmark_dir}/{algo_name}/{algo_name}.cpp"
-    with open(code_path, "r") as code_file:
-        CODE = "\n" + code_file.read()
-
-    TASK_PIPELINE_PROMPT_COMPLETE = _SYSTEM_PROMPT + TASK_PIPELINE_PROMPT + CODE
-    TASK_PIPELINE_PROMPT_COMPLETE += (
-        TASK_PIPELINE_STRATEGY_PROMPT_4  # add strategy hint
-    )
-    print(TASK_PIPELINE_PROMPT_COMPLETE)
-    return TASK_PIPELINE_PROMPT_COMPLETE
-
-
-# split sub function in the code
-def gen_sub_task_pipeline_prompt(func_description, func_content):
-    _SYSTEM_PROMPT = SYSTEM_PROMPT.replace("{ALGO_NAME}", algo_name)
-    _SYSTEM_PROMPT = _SYSTEM_PROMPT.replace("{FUNCTION_DESCRIPTION}", func_description)
-
-    TASK_PIPELINE_PROMPT_COMPLETE = (
-        _SYSTEM_PROMPT + TASK_PIPELINE_FOR_SINGLE_FUNCTION_PROMPT + func_content
-    )
-    TASK_PIPELINE_PROMPT_COMPLETE += (
-        TASK_PIPELINE_STRATEGY_PROMPT_4  # add strategy hint
-    )
-    print(TASK_PIPELINE_PROMPT_COMPLETE)
-    return TASK_PIPELINE_PROMPT_COMPLETE
-
-
-def extract_content_and_save(
-    chat_completion, prompt_content, model_name, sub_func=False
-):
-    if model_name in ["gpt-4-1106-preview", "gpt-4"]:
-        model_name = "gpt4"
-    else:
-        model_name = "gpt3.5"
-    cur_time = time.strftime("%y%m%d_%H%M", time.localtime())
-    chat_file_path = f"{pipeline_log_path}/{model_name}/{algo_name}/pipeline_{model_name}_{algo_name}_{cur_time}.txt"
-    code_file_path = f"{pipeline_log_path}/{model_name}/{algo_name}/pipeline_{model_name}_{algo_name}_{cur_time}.cpp"
-
-    if sub_func:
-        chat_file_path = f"{pipeline_log_path}/{model_name}/{algo_name}/sub_pipeline_{model_name}_{algo_name}_{cur_time}.txt"
-        code_file_path = f"{pipeline_log_path}/{model_name}/{algo_name}/sub_pipeline_{model_name}_{algo_name}_{cur_time}.cpp"
-
-    # save chat completion
-    with open(chat_file_path, "w") as chat_file:
-        chat_file.write(str(chat_completion))
-        chat_file.write("\n\n====================================\n\n")
-        chat_file.write(prompt_content)
-
-    # extract code from ``` ``` and save
-    content = chat_completion.choices[0].message["content"]
-    match = re.search(r"\`\`\`(.*?)\`\`\`", content, re.DOTALL)
-    if match:
-        code_content = match.group(1)
-        print(code_content)
-        with open(code_file_path, "w") as code_file:
-            code_file.write(code_content)
-
-
-def run_task_pipeline(func_description):
-    prompt = gen_task_pipeline_prompt(func_description)
-    model_name = "gpt-4-1106-preview"
-    task_pipeline_completion = openai.ChatCompletion.create(
-        model=model_name, messages=[{"role": "user", "content": prompt}]
-    )
-    print(task_pipeline_completion)
-    extract_content_and_save(task_pipeline_completion, prompt, model_name, algo_name)
-
-
-def run_sub_task_pipeline(func_description, func_content):
-    prompt = gen_sub_task_pipeline_prompt(func_description, func_content)
-    model_name = "gpt-4-1106-preview"
-    sub_task_pipeline_completion = openai.ChatCompletion.create(
-        model=model_name, messages=[{"role": "user", "content": prompt}]
-    )
-    print(sub_task_pipeline_completion)
-    extract_content_and_save(
-        sub_task_pipeline_completion, prompt, model_name, sub_func=True
-    )
+def pre_processing_pipeline(func_content, target):
+    """This function transforms the given code by performing two main transformations:
+        1. Convert parallel loop variables (e.g., OpenMP, CUDA) into standard C for loops.
+        2. Convert SIMD tensor operations into scalar for-loop based calculations.
+    :param func_content: The content of the function (code) to be transformed.
+    :return: Transformed code after applying the two transformations."""
+    TRANS_DESCRIPTION = ""
+    for trans in OPT_LIST:
+        prompt_name = (
+            f"{trans}_PROMPT_{target}"
+            if trans != "DETENSORIZATION"
+            else f"{trans}_PROMPT"
+        )
+        prompt_content = globals()[prompt_name]
+        TRANS_DESCRIPTION += prompt_content
+    return TRANS_DESCRIPTION
 
 
 if __name__ == "__main__":
     func_content = """
-    extern "C" __mlu_global__ void tanh_kernel0(float* input0, float* active_tanh_210) {
-            __nram__ float input0_local_nram[640];
-            for (int clusterId = 0; clusterId < 4; clusterId++) {
-                for (int coreId = 0; coreId < 4; coreId++) {
-                __memcpy(((float *)input0_local_nram + (0)), ((float *)input0 + (((((int)clusterId) * 2560) + (((int)coreId) * 640)))), 2560, GDRAM2NRAM);
-                __bang_active_tanh(((float *)input0_local_nram + (0)), ((float *)input0_local_nram + (0)), 640);
-                __memcpy(((float *)active_tanh_210 + (((((int)clusterId) * 2560) + (((int)coreId) * 640)))), ((float *)input0_local_nram + (0)), 2560, NRAM2GDRAM);
-                }
-            }
-        }
+    extern "C" __mlu_global__ void tanh(float* input0, float* active_tanh_210) {
+        __nram__ float input0_local_nram[640];
+        __memcpy(((float *)input0_local_nram + (0)), ((float *)input0 + (((((int)clusterId) * 2560) + (((int)coreId) * 640)))), 2560, GDRAM2NRAM);
+        __bang_active_tanh(((float *)input0_local_nram + (0)), ((float *)input0_local_nram + (0)), 640);
+        __memcpy(((float *)active_tanh_210 + (((((int)clusterId) * 2560) + (((int)coreId) * 640)))), ((float *)input0_local_nram + (0)), 2560, NRAM2GDRAM);
+    }
     """
-    run_task_pipeline(func_content)
+    pre_processing_pipeline(func_content, target="BANG")
