98a6f49d70a95a6ff0fe721f1379aa57e8908b60
ShouyangDong <shouyang.dong@gmail.com>
2024-10-21 17:47:59 +0800

    add cuda gelu test case

---
+++ benchmark/data/cuda_code_test/gelu_5_12_23_128.cu
@@ -0,0 +1,29 @@
+
+float geluf(float x) {
+    return 0.5 * x * (1 + tanh(sqrt(2 / M_PI) * (x + 0.044715 * pow(x, 3))));
+}
+
+__global__ void __launch_bounds__(1024) gelu(float* __restrict__ A, float* __restrict__ compute) {
+  if (((((int)blockIdx.x) * 1024) + ((int)threadIdx.x)) < 176640) {
+    compute[((((int)blockIdx.x) * 1024) + ((int)threadIdx.x))] = geluf(A[((((int)blockIdx.x) * 1024) + ((int)threadIdx.x))]);
+  }
+}
+
+extern "C" void gelu_kernel(float *C, float *A, int size) {
+  float *d_A, *d_C;
+
+  cudaMalloc(&d_A, size * sizeof(float));
+  cudaMalloc(&d_C, size * sizeof(float));
+
+  cudaMemcpy(d_A, A, size * sizeof(float), cudaMemcpyHostToDevice);
+
+  dim3 blockSize(1024);
+  dim3 numBlocks((size + 1024 - 1) / 1024);
+
+  gelu<<<numBlocks, blockSize>>>(d_A, d_C);
+
+  cudaMemcpy(C, d_C, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(d_A);
+  cudaFree(d_C);
+}
