4e5c5b45961bae3ca1b854a08a002630a299b486
ShouyangDong <shouyang.dong@gmail.com>
2024-11-11 11:03:02 +0800

    fix loop_fusion

--- falcon/smt/loop_transformation/loop_fusion.py
+++ falcon/smt/loop_transformation/loop_fusion.py
@@ -1,4 +1,5 @@
 from pycparser import c_ast, c_generator, c_parser
+
 from falcon.smt.util import NodeTransformer
 
 
@@ -14,9 +15,19 @@ class LoopNestFusionVisitor(c_ast.NodeVisitor):
                 node.cond = fused_loop.cond
                 node.next = fused_loop.next
                 node.stmt = fused_loop.stmt
-                self.update_loop_index(node.stmt, outer_var=node.init.decls[0].name, inner_var=nested_loop.init.decls[0].name, inner_bound=nested_loop.cond.right.value)
+                self.update_loop_index(
+                    node.stmt,
+                    outer_var=node.init.decls[0].name,
+                    inner_var=nested_loop.init.decls[0].name,
+                    inner_bound=nested_loop.cond.right.value,
+                )
                 # 更新初始化部分，将变量名改为 i_j_fused
-                node.init.decls[0].name = node.init.decls[0].name + "_" + nested_loop.init.decls[0].name + "_fused"
+                node.init.decls[0].name = (
+                    node.init.decls[0].name
+                    + "_"
+                    + nested_loop.init.decls[0].name
+                    + "_fused"
+                )
                 node.init.decls[0].type.declname = node.init.decls[0].name
                 node.cond.left.name = node.init.decls[0].name
                 node.next.expr.name = node.init.decls[0].name
@@ -28,18 +39,20 @@ class LoopNestFusionVisitor(c_ast.NodeVisitor):
         # 创建新的循环变量和边界，合并两个循环
         fused_init = outer_loop.init
         fused_cond = c_ast.BinaryOp(
-            op='<',
+            op="<",
             left=c_ast.ID(name=fused_init.decls[0].name),
-            right=c_ast.Constant(type='int', value=str(int(outer_loop.cond.right.value) * int(inner_loop.cond.right.value)))
+            right=c_ast.Constant(
+                type="int",
+                value=str(
+                    int(outer_loop.cond.right.value) * int(inner_loop.cond.right.value)
+                ),
+            ),
         )
         fused_next = outer_loop.next
         # 将嵌套循环的主体语句调整为单层循环
         fused_stmt = c_ast.Compound(inner_loop.stmt.block_items)
         fused_loop = c_ast.For(
-            init=fused_init,
-            cond=fused_cond,
-            next=fused_next,
-            stmt=fused_stmt
+            init=fused_init, cond=fused_cond, next=fused_next, stmt=fused_stmt
         )
         return fused_loop
 
@@ -47,35 +60,51 @@ class LoopNestFusionVisitor(c_ast.NodeVisitor):
         # 更新循环体中的索引表达式，将 i + (10 * j) 转换为单一索引 i_j_fused
         class IndexUpdater(c_ast.NodeVisitor):
             def visit_ArrayRef(self, array_node):
-                if isinstance(array_node.subscript, c_ast.BinaryOp) and array_node.subscript.op == '+':
+                if (
+                    isinstance(array_node.subscript, c_ast.BinaryOp)
+                    and array_node.subscript.op == "+"
+                ):
                     left = array_node.subscript.left
                     right = array_node.subscript.right
                     if (
-                        isinstance(right, c_ast.ID) and right.name == inner_var
-                        and isinstance(left, c_ast.BinaryOp) and left.op == '*'
-                        and isinstance(left.right, c_ast.Constant) and left.right.value == str(inner_bound)
-                        and isinstance(left.left, c_ast.ID) and left.left.name == outer_var
+                        isinstance(right, c_ast.ID)
+                        and right.name == inner_var
+                        and isinstance(left, c_ast.BinaryOp)
+                        and left.op == "*"
+                        and isinstance(left.right, c_ast.Constant)
+                        and left.right.value == str(inner_bound)
+                        and isinstance(left.left, c_ast.ID)
+                        and left.left.name == outer_var
                     ):
                         # 替换索引为单一变量 i_j_fused
-                        fused_index = c_ast.ID(name=outer_var + "_" + inner_var +"_fused")
+                        fused_index = c_ast.ID(
+                            name=outer_var + "_" + inner_var + "_fused"
+                        )
                         array_node.subscript = fused_index
-                #TODO:add check
-                # elif isinstance(array_node.subscript, c_ast.BinaryOp) and array_node.subscript.op == '*':
-                #     left = array_node.subscript.left
-                #     right = array_node.subscript.right
-                #     if (
-                #         isinstance(left, c_ast.ID) and left.name == outer_var
-                #         and isinstance(right, c_ast.Constant) and right.value == str(inner_bound)
-                #     ):
-                #         print(left)
-                #         # 替换索引为单一变量 i_j_fused
-                #         fused_index = c_ast.ID(name=outer_var + "_" + inner_var +"_fused")
-                #         array_node.subscript = fused_index    
+
+                elif (
+                    isinstance(array_node.name, c_ast.BinaryOp)
+                    and array_node.subscript.name == inner_var
+                ):
+                    right = array_node.name.right
+                    if (
+                        isinstance(right, c_ast.BinaryOp)
+                        and right.left.expr.name == outer_var
+                        and right.op == "*"
+                        and isinstance(right.right, c_ast.Constant)
+                        and right.right.value == str(inner_bound)
+                    ):
+                        fused_index = c_ast.ID(
+                            name=outer_var + "_" + inner_var + "_fused"
+                        )
+                        array_node.subscript = fused_index
+                        array_node.name = array_node.name.left
 
                 self.generic_visit(array_node)
 
         updater = IndexUpdater()
         updater.visit(node)
+
 
 def ast_loop_fusion(c_code):
     # 解析 C 代码
@@ -87,6 +116,7 @@ def ast_loop_fusion(c_code):
     # 访问 AST，合并可以合并的嵌套 for 循环
     visitor.visit(ast)
     return generator.visit(ast)
+
 
 if __name__ == "__main__":
     # 含有嵌套 for 循环的 C 代码
@@ -103,20 +133,19 @@ if __name__ == "__main__":
     final_code = ast_loop_fusion(c_code)
     print(final_code)
 
+    c_code = """
+    void add(float *lhs, float *rhs, float *add_1935)
+    {
+        for (int coreId = 0; coreId < 4; ++coreId)
+        {
+            for (int i = 0; i < 1024; i++)
+            {
+            (((float *) add_1935) + (((int) coreId) * 1024))[i] = (((float *) lhs) + (((int) coreId) * 1024))[i] + (((float *) rhs) + (((int) coreId) * 1024))[i];
+            }
 
-    # c_code = """
-    # void add(float *lhs, float *rhs, float *add_1935)
-    # {
-    #     for (int coreId = 0; coreId < 4; ++coreId)
-    #     {
-    #         for (int i = 0; i < 1024; i++)
-    #         {
-    #         (((float *) add_1935) + (((int) coreId) * 1024))[i] = (((float *) lhs) + (((int) coreId) * 1024))[i] + (((float *) rhs) + (((int) coreId) * 1024))[i];
-    #         }
-
-    #     }
-    # }
-    # """
-    # # 合并循环后的最终代码
-    # final_code = ast_loop_fusion(c_code)
-    # print(final_code)
+        }
+    }
+    """
+    # 合并循环后的最终代码
+    final_code = ast_loop_fusion(c_code)
+    print(final_code)
